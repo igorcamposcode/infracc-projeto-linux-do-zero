ce0fc213bb99b8ca7ae943c4df272f42836b378c
Igor Campos <64698809+igorcamposcode@users.noreply.github.com>
2023-03-03 20:11:58 -0300

    Adicionando projeto do bootcamp linux do zero

---
+++ script/infracc_projetoservidorwebapache.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Executando operações"
+echo "Instalando Atualizaçãos do sistema..."
+
+apt-get update -y
+apt-get upgrade -y
+
+echo "Finalizando atualizações do sistema"
+echo "Instalando apache2 e unzip..."
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Finalizando instalações"
+echo "Realizando configurações..."
+
+cd /scripts/meus_scripts/
+
+wget url https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+cd /sciprts/meus_scripts/linux-site-dio/
+cp -R * /var/www/html
+
+echo "Finalizando configurações"
+
